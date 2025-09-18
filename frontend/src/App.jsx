e463f3b2a32115cabbbf32f1c6f186c3e88c87c7
kilian-zindel <kilianzindel@proton.me>
2025-09-18 12:06:43 -0500

    fixed config, switched to tailwind 4, added form validation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+// pages 
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+// components 
+import Navbar from './components/Navbar'
+import Loader from './components/Loader.jsx'
+import { Toaster } from 'react-hot-toast'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore.js'
+import { useEffect } from 'react'
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore() 
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      ></Toaster>
+      <div className="flex-col h-screen">
+        <Navbar />
+        <div className="h-full">
+          <Routes>
+            <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} /> 
+            <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} /> 
+            {/* <Route path='/signup' element={<SignUpPage />} /> */}
+            <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} /> 
+            <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} /> 
+            <Route path="/settings" element={<SettingsPage />} /> 
+          </Routes>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
+import { MessageSquare, Eye, EyeOff, Loader2 } from 'lucide-react'
+import { Link } from 'react-router-dom';
+import AuthImagePattern from '../components/AuthImagePattern';
+import { toast } from 'react-hot-toast';
 
 const SignUpPage = () => {
-
-  /* TODO: 
-    - [ ] Disable Sign Up Button when isSigningUp
-    - [ ] Write Validate Form Function 
-  */
 
   const { signup, isSigningUp } = useAuthStore(); 
 
   const [isLoading, setIsLoading] = useState(false); 
-    // initially true, until page checks auth 
-    // NO, false because loading screen is handled in App.jsx?
-    // only need loading while creating new user!
   const [showPassword, setShowPassword] = useState(false);
-
-  // condense these refs into an object 
-  const nameInputRef = useRef(null);
-  const emailInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
 
   const [formData, setFormData] = useState({
     // use "" for consistent datatype, makes it a "controlled component"
@@ -29,67 +20,157 @@ const SignUpPage = () => {
     'password': "", 
   })
 
-  const updateFormData = () => {
-    // called when any form input changes
-    // setFormData({
-    //   'fullName': nameInputRef.current.value,
-    //   'email': emailInputRef.current.value, 
-    //   'password': passwordInputRef.current.value, 
-    // })
-  }
-
-  useEffect(() => {
-    // update DOM with new form data
-      // nameInputRef.current.value = formData.name
-      // emailInputRef.current.value = formData.email
-      // passwordInputRef.current.value = formData.password
-  }, [formData])
-
   const validateForm = () => {
-    // validate user inputs i.e. missing input of password length etc 
-    // update some error state, or alert error message? or highlight a DOM element
-    // return True or False
+    const {fullName, email, password} = formData;
+
+    // validate name form 
+    if (fullName.trim() === "" || email.trim() === "" || password.trim() === ""){
+      toast.error("All inputs are required.")
+      console.log("error!")
+      return false
+    }
+
+    if (password.trim().length < 6){
+      toast.error("Password must be at least 6 characters")
+      return false
+    }
+
+    return true;
   }
 
   const handleSubmit = (e) => {
     e.preventDefault(); 
 
     if (validateForm(e)) {
-      // submit
+      console.log(formData) 
     }
   }
 
   return (
-    <div className="grid grid-cols-2 h-full"> 
-      {/* Left Hand Side */}
-      <div className="col-1 h-full bg-base-300 flex justify-center items-center">
-        <div className="bg-base-200">
-          <h1 className="text-4xl font-bold text-primary text-center">Create Account</h1>
-          <p className="mt-2 text-lg text-base-content/70 text-center">Get started with your free account</p>
-          
-          {/* FIELD SET  */}
-          <fieldset className="fieldset border-none rounded-box border w-full p-0">
+    <div className="min-h-screen grid lg:grid-cols-2"> 
+      {/* LEFT HAND SIDE */}
+      <div className="h-full flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
 
-            <label className="label mt-4">Full Name</label>
-            <input type="text" className="input w-full" placeholder="John Doe" />
+          {/* LOGO AND HEADING */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col gap-2 items-center group">
+              {/* LOGO */}
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center
+                group-hover:bg-primary/20 transition-colors">
+                <MessageSquare className="size-6 text-primary" /> 
+              </div>
+              {/* HEADING  */}
+              <h1 className="text-2xl text-primary font-bold mt-2">Create Account</h1>
+              <p className="text-base-content opacity-60">Get started with your free account</p>
+            </div>
+          </div>
 
-            <label className="label mt-4">Email</label>
-            <input type="text" className="input w-full" placeholder="you@example.com" />
+          {/* FORM  */}
+          <form onSubmit={handleSubmit}>
+          <fieldset className="fieldset">
 
-            <label className="label mt-4">Password</label>
-            <input type="password" className="input w-full" placeholder="••••••••" />
+            {/* NAME INPUT  */}
+            <label className="label"><span className="label-text font-medium">Full Name</span></label>
+            <label className="input input-bordered w-full flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+              <input 
+                type="text" 
+                className="grow" 
+                placeholder="John Doe" 
+                value={formData.fullName}
+                onChange={(e) => setFormData({ ...formData, fullName: e.target.value})}
+              />
+            </label>
 
-            <input type="submit" className="btn btn-primary mt-4" value="Create Account"></input>
+            {/* EMAIL INPUT */}
+            <label className="label"><span className="label-text font-medium mt-4">Email</span></label>
+            <label className="input input-bordered w-full flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path
+                  d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input 
+                type="text" 
+                className="grow" 
+                placeholder="you@example.com" 
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value})}
+              />
+            </label>
 
+            {/* PASSWORD INPUT */}
+            <label className="label"><span className="label-text font-medium mt-4">Password</span></label>
+            <label className="input input-bordered w-full flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd" />
+              </svg>
+              <input 
+                type={showPassword ? "text" : "password"}
+                className="grow" 
+                placeholder="••••••••" 
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value})}
+              />
+              <button type="button"
+                className="h-4 w-4 opacity-70"
+                onClick={() => setShowPassword(!showPassword)}>
+                { !showPassword && <Eye size="16"/>}
+                { showPassword && <EyeOff size="16"/>}
+              </button>
+            </label>
+
+            {/* SUBMIT BUTTON  */}
+            <button type="submit" 
+              className="btn btn-primary w-full mt-4" 
+              disabled={isSigningUp}
+              onClick={handleSubmit}>
+                { isSigningUp ? (
+                  <><Loader2 className="size-5 animate-spin" />Loading...</>
+                ) : (
+                  <>Create Account</>
+                ) }
+            </button>
           </fieldset>
+          </form>
 
-
+          {/* LINK TO LOGIN PAGE */}
+          <div className="text-center">
+            <p className="text-base-content">
+                <span className="opacity-60">Already have an account?{" "}</span>
+                <Link to="/login" className="link link-primary">
+                  Sign In
+                </Link>
+            </p>
+          </div>
         </div>
       </div>
 
       {/* Right Hand Side */}
-      <div className="col-2 h-full bg-base-200">
-      </div>
+      <AuthImagePattern 
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
     </div>
   )
 }
